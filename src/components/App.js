e61776af91dc099f5428d2777514c08e5bbf66ef
AlejandraSC <alejandrasalmeron6@gmail.com>
2020-11-11 13:38:23 +0100

    Change fillProps

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import Footer from './Footer';
 
 //Create props to pass to Preview
 const fillProps = {
-  name: '',
-  job: '',
-  profileImage: './images/lisa_simpson.png',
+  name: 'Lisa Simpsons',
+  job: 'Front-end developer',
+  profileImage: '',
   email: '',
   phone: '',
   linkedin: '',
