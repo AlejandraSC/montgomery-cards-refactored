59b8e6af8e5ce50c2a0075d632ac4bd290d3a4e0
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-11-19 15:10:43 +0100

    changes

--- src/components/AppFolder/App.js
+++ src/components/AppFolder/App.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Header from '../HeaderFolder/Header';
-import Main from '../MainFolder/Main';
-import Footer from '../FooterFolder/Footer';
-
-//Create props to pass to Preview
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      job: '',
-      profileImage: '',
-      email: '',
-      phone: '',
-      linkedin: '',
-      github: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  handleChange(inputValue, inputValueId) {
-    this.setState({
-      [inputValueId]: inputValue
-    });
-  }
-
-  handleReset() {
-    this.setState({
-      name: '',
-      job: '',
-      profileImage: '',
-      email: '',
-      phone: '',
-      linkedin: '',
-      github: ''
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <Header></Header>
-        <Main
-          data={this.state}
-          handleChange={this.handleChange}
-          handleReset={this.handleReset}
-        ></Main>
-        <Footer></Footer>
-      </>
-    );
-  }
-}
-
-export default App;
-=======
 import React from 'react';
 import Header from '../HeaderFolder/Header';
 import Main from '../MainFolder/Main';
@@ -123,4 +64,3 @@ class App extends React.Component {
 }
 
 export default App;
->>>>>>> d4946cc37b7f8bee6a311f8de4f09483f0d63cbc
