ecc14eda1888e906273edfc93e6697d6dce2bd6e
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-11-27 11:03:52 +0100

    add card

--- src/components/AppFolder/App.js
+++ src/components/AppFolder/App.js
@@ -17,11 +17,15 @@ class App extends React.Component {
       phone: '',
       linkedin: '',
       github: '',
-      design: '1'
+      design: '1',
+      apiSuccess: false,
+      apiCardUrl: '',
+      apiError: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.handleChangeRadio = this.handleChangeRadio.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
   }
 
   handleChange(inputValue, inputValueId) {
@@ -48,6 +52,36 @@ class App extends React.Component {
     });
   }
 
+  // fetch
+  sendRequest() {
+    const apiData = {
+      name: this.state.name,
+      job: this.state.job,
+      phone: this.state.phone,
+      email: this.state.email,
+      linkedin: this.state.linkedin,
+      github: this.state.github,
+      palette: this.state.design,
+      photo: this.state.profileImage
+    };
+    GetApi(apiData).then((response) => {
+      if (response.success === true) {
+        this.setState({
+          apiSuccess: true,
+          apiCardUrl: response.cardURL,
+          apiError: ''
+        });
+        console.log(this.state.apiCardUrl);
+      } else {
+        this.setState({
+          apiSuccess: false,
+          apiCardUrl: '',
+          apiError: response.error
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <>
@@ -57,6 +91,7 @@ class App extends React.Component {
           handleChange={this.handleChange}
           handleReset={this.handleReset}
           handleChangeRadio={this.handleChangeRadio}
+          sendRequest={this.sendRequest}
         ></Main>
         <Footer></Footer>
       </>
